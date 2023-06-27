484575e29c58218d659a8d09518223d44805c95a
tleivy <tomerleivy1@gmail.com>
2023-06-27 17:42:03 +0300

    refactoring

--- run_example.py
+++ run_example.py
@@ -1,10 +1,10 @@
 from PersonDetector import *
 
-detector = PersonDetector("model/keras_model.h5", "model/labels.txt")
+detector = PersonDetector(r"model/keras_model.h5", r"model/labels.txt")
 
 while True:
-    detector.detect("your_label")  # todo: use TL or omri for the example
-    # Listen to the keyboard for presses.
+    detector.detect("your_label")  # for example: "person"
+
     keyboard_input = cv2.waitKey(1)
     if keyboard_input == 27:  # key = Escape
         break
